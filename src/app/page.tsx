5082cdab45d32f7c3e9617a1ab8a73d3ab0ed4c3
Asuka-Aozora <azdin1411@gmail.com>
2025-12-05 09:43:32 +0700

    feat: integrate Inngest for workflow management and add new API routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
+"use client";
+
 import { requireAuth } from "@/lib/lib-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers(); 
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued");
+    }
+  }));
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
-      <div>
-      {JSON.stringify(data, null, 2)}
-      </div>
-      <LogoutButton/>
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
+      <LogoutButton />
     </div>
   );
 };
---
+++ src/inngest/function.ts
@@ -0,0 +1,23 @@
+import prisma from "@/lib/db";
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    // Fetching the youtube video
+    await step.sleep("fetching", "5s");
+    // Transcribing
+    await step.sleep("transcribing", "5s");
+    // Sending transcription to AI
+    await step.sleep("sending-to-ai", "5s");
+    
+      await step.run("create-workflow", () => {
+        return prisma.workflow.create({
+            data: {
+                name: "workflow-prismw-from-inggest",
+            }
+        })
+    })
+  }
+);
